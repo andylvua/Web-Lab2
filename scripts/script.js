e6a6be6e45f1b543433b35c707855212f64af050
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-08-22 18:59:51 +0300

    Updated navigation to scrolling adaptive

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,31 @@
-$('a').click(function() {
-    console.log('Selected section: ' + $(this).attr('href'));
-    $('a').removeClass('active');
-    $(this).addClass('active');
-});
+// Make home section active by default
+let home = document.querySelector("nav a");
+home.classList.add("active");
+
+window.onscroll = () => {
+    let current = "";
+
+    const sections = document.querySelectorAll("section");
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        let header = $("header");
+        let headerMarginTop = parseInt(header.css('margin-top'));
+        let headerHeight = header.outerHeight();
+
+        if (sectionTop - pageYOffset <= headerHeight) {
+            current = section.getAttribute("id");
+        }
+        if (pageYOffset <= headerMarginTop) {
+            current = "home";
+        }
+    });
+
+
+    const links = document.querySelectorAll("nav a");
+    links.forEach((element) => {
+        element.classList.remove("active");
+        if (element.classList.contains(current)) {
+            element.classList.add("active");
+        }
+    });
+};
