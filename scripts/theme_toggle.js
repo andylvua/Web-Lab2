9800239e1458c68160e124c8b73e878a6e0292f2
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2022-08-25 14:28:06 +0300

    Minor code style fixes

--- scripts/theme_toggle.js
+++ scripts/theme_toggle.js
@@ -1,7 +1,7 @@
 let root = $('html');
+
 $('#theme_toggle').click(function() {
     console.log('Toggling theme');
-    console.log(root.attr('class'));
 
     if (root.attr('class') === 'light') {
         $('html').attr('class', 'dark');
